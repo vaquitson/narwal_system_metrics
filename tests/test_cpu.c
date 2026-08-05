91e4964bc687ef20b4cc9d0a506fb5924438bed7
Narval <vaquitson@gmail.com>
2026-08-05 00:46:19 -0400

    adding proper testing

--- tests/test_cpu.c
+++ tests/test_cpu.c
@@ -1,14 +1,90 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
 #include "narwal_cpu.h"
+
+#define EXE_TEST(n)                                                   \
+    do {                                                              \
+        rc = test_##n();                                              \
+        if (rc != 0) {                                                \
+            printf("test_" #n ": Failed, exit code %d\n", rc);        \
+        } else {                                                      \
+            printf("test_" #n ": Pass\n");                            \
+        }                                                             \
+    } while (0) \
+
+
+int test_1(void)
+{
+  int stat_fd;
+
+  NarwalCpuTime_t busy_time_arr[] = {
+    103026,  // cpu
+    12253,   // cpu0
+    12973,   // cpu1
+    16033,   // cpu2
+    11577,   // cpu3
+    12347    // cpu4
+  };
+
+  NarwalCpuTime_t idle_time_arr[] = {
+    3115217, // cpu
+    389896,  // cpu0
+    389192,  // cpu1
+    386278,  // cpu2
+    390652,  // cpu3
+    389891   // cpu4
+  };
+
+  NarwalCpu cpu_arr[6];
+  NarwalCpu *cur_cpu_p = cpu_arr;
+  NarwalCpuTime_t idle_time;
+  NarwalCpuTime_t busy_time;
+
+  stat_fd = open("/proc/stat", O_RDONLY);
+  if (stat_fd < 0)
+    return -1;
+  close(stat_fd);
+
+  stat_fd = open("tests/test_files/stat", O_RDONLY);
+  if (stat_fd < 0){
+    printf("errno: %s\n", strerror(errno));
+    return -2;
+  }
+
+  *priv_narwal_cpu_get_fd() = stat_fd;
+
+  for (int i = -1; i < 5; i++){
+    cur_cpu_p = &cpu_arr[i+1];
+    narwal_cpu_init(cur_cpu_p, i);
+
+    idle_time = narwal_cpu_idle_time(cur_cpu_p);
+    busy_time =narwal_cpu_busy_time(cur_cpu_p);
+
+    if (busy_time != busy_time_arr[i+1]){
+      printf("Expect: %lld But get %lld",busy_time_arr[i+1], busy_time);
+      return -3;
+    }
+
+    if (idle_time != idle_time_arr[i+1]){
+      printf("Expect: %lld But get %lld",idle_time_arr[i+1], idle_time);
+      return -4;
+    }
+  }
+
+  return 0;
+
+}
 
 int test_cpu_percentage(void){
   int stat_fd;
   int rc = 0;
   NarwalCpu cpu = {0};
-  NARWAL_CPU_TIME_T time = 0;
+  NarwalCpuTime_t time = 0;
 
-
-  printf("fd: %d\n", *priv_narwal_cpu_get_fd());
 
   rc = narwal_cpu_init(&cpu, NARWAL_CPU_GENERAL_CPU);
   if (rc < 0) {
@@ -28,12 +104,14 @@ int test_cpu_percentage(void){
   time = narwal_cpu_busy_time(&cpu); 
   printf("busy time: %lld\n", time);
 
-
   return 0;
 }
 
 
-int main(void){
-  test_cpu_percentage();
+int main(void)
+{
+  int rc = 0;
+
+  EXE_TEST(1);
   return 0;
 }
